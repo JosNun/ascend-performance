e4b332a49c1dc16cf24e1d9eea6191274ec6a6d3
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-08-26 17:13:25 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import 'typeface-pt-sans';
 
 import Navbar from './Nav/Nav';
-import Header from './Header/Header';
-import CardSlider from './Cards/CardSlider';
-import Card from './Cards/Card';
-import TextView from './common/TextView';
-import TextInput from './common/TextInput';
-
-import graphImg from './assets/images/icons/graph.svg';
-import clockImg from './assets/images/icons/clock.svg';
-import infiniteImg from './assets/images/icons/infinite.svg'
-
-import Footer from './Footer/Footer';
+import Home from './Home/Home'
+import About from './About/About';
 
 const Body = styled.div`
   margin-top: 50px;
@@ -22,36 +14,17 @@ const Body = styled.div`
   font-family: 'PT Sans';
 `
 
-const BodyHeader = styled.h1`
-  color: #242424;
-  text-align: center;
-`
-
 class App extends Component {
   render() {
     return (
-      <Body>
-        <Navbar />
-        <Header />
-        <CardSlider>
-          <Card headImage={clockImg}>
-            By using state of the art technology and AI, Ascend personalizes your workouts to maximize gain and save you time.
-          </Card>
-          <Card headImage={graphImg} >
-            Ascend Performance uses the latest in performance tracking technology, enabling you to track your workouts, and synchronize them effortlessly across all of your devices.
-          </Card>
-          <Card headImage={infiniteImg}>
-            Workout sharing enables a bit of friendly competition which has been proven to boost motivation and workout efficiency
-          </Card>
-        </CardSlider>
-        <TextView>
-          By combining the sensors that are already in your smartphone with state of the art AI and machine learning, Ascend takes fitness tracking to the next level. Sign up below to be one of the first to be able to make use of this new technology, and help test and train the future of personal fitness.
-        </TextView>
-        
-        <BodyHeader>Join the Revolution</BodyHeader>
-        <TextInput placeholder="Enter your email..."></TextInput>
-        <Footer />
-      </Body>
+      <Router>
+        <Body>
+          <Navbar />
+          
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Body>
+      </Router>
     );
   }
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Header from './Header';
+import Card from '../Cards/Card';
+import CardSlider from '../Cards/CardSlider';
+import TextView from '../common/TextView';
+import TextInput from '../common/TextInput';
+import Footer from '../common/Footer';
+
+import graphImg from '../assets/images/icons/graph.svg';
+import clockImg from '../assets/images/icons/clock.svg';
+import infiniteImg from '../assets/images/icons/infinite.svg'
+
+const BodyHeader = styled.h1`
+  color: #242424;
+  text-align: center;
+`
+
+export default () => {
+  return (
+    <React.Fragment>
+        <Header />
+        <CardSlider>
+          <Card headImage={clockImg}>
+            By using state of the art technology and AI, Ascend personalizes your workouts to maximize gain and save you time.
+          </Card>
+          <Card headImage={graphImg} >
+            Ascend Performance uses the latest in performance tracking technology, enabling you to track your workouts, and synchronize them effortlessly across all of your devices.
+          </Card>
+          <Card headImage={infiniteImg}>
+            Workout sharing enables a bit of friendly competition which has been proven to boost motivation and workout efficiency
+          </Card>
+        </CardSlider>
+        <TextView>
+          By combining the sensors that are already in your smartphone with state of the art AI and machine learning, Ascend takes fitness tracking to the next level. Sign up below to be one of the first to be able to make use of this new technology, and help test and train the future of personal fitness.
+        </TextView>
+        
+        <BodyHeader>Join the Revolution</BodyHeader>
+        <TextInput placeholder="Enter your email..."></TextInput>
+        <Footer />
+      </React.Fragment>
+  )
+}
