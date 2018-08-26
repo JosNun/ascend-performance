6fd4ee3b6b7ddc90f0b749e32bc6722b5fcdbe9d
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-08-26 17:17:44 -0400

    Add about page

---
+++ src/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import TextView from '../common/TextView';
+
+export default () => {
+  return (
+    <TextView>
+      <h1>Sorry to disappoint but..</h1>
+    </TextView>
+  )
+}
