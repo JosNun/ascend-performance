98243652892d2b5a49e3e358a7baf83d7fc4b78f
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-08-26 15:35:23 -0400

    Enable responsive slider

--- src/Cards/CardSlider.js
+++ src/Cards/CardSlider.js
@@ -1,22 +1,40 @@
 import React from 'react'
+import styled from 'styled-components';
 import Swiper from 'react-id-swiper/lib/custom';
 
 import 'react-id-swiper/src/styles/css/swiper.css';
 
+const SliderContainer = styled.div`
+  @media screen and (min-width: 720px) {
+    margin: 0 15px;
+  }
+`;
 
 export default props => {
   
   const params = {
-    slidesPerView: 1.5,
-    loopedSlides: props.children.length,
-    loop: true,
+    slidesPerView: 3,
+    initialSlide: 1,
+    loop: false,
+    loopedSlides: 0,
     centeredSlides: true,
     spaceBetween: 15,
+    allowTouchMove: false,
+    breakpoints: {
+      720: {
+        allowTouchMove: true,
+        slidesPerView: 1.5,
+        loopedSlides: props.children.length,
+        loop: true,
+      }
+    }
   }
   
   return (
-    <Swiper {...params}>
-      {props.children}
-    </Swiper>
+    <SliderContainer>
+      <Swiper {...params}>
+        {props.children}
+      </Swiper>
+    </SliderContainer>
   )
 }
