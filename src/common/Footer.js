addf6d5cb0a30b24183972f09c72052f0b84987e
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-08-28 15:45:34 -0400

    Add link to portfolio in footer

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
   position: absolute;
   right: 0;
   left: 0;
-  
+
   margin-top: 30px;
   padding: 15px;
-  
-  background-color: #242424;
-  color: #fff;
-  text-align: center;
-`
 
-export default () => {
-  return (
-    <StyledFooter>
-      © 2018 Josiah Nunemaker
-    </StyledFooter>
-  )
-}
+  background-color: #242424;
+  color: #808080;
+  text-align: center;
+`;
+
+const FooterLink = styled.a`
+  color: #fff;
+`;
+
+export default () => (
+  <StyledFooter>
+    © 2018{' '}
+    <FooterLink
+      href="https://josnun.github.io"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Josiah Nunemaker
+    </FooterLink>
+  </StyledFooter>
+);
