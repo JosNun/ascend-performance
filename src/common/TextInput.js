af8ff580386015abca73aa0861453615f4f6fc69
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-08-28 21:10:29 -0400

    Enable cursor pointer for form submit buttons

--- src/common/TextInput.js
+++ src/common/TextInput.js
@@ -51,7 +51,7 @@ const Button = styled.input.attrs({
   border-width: 0 0 0 2px;
   font-weight: 700;
   font-style: italic;
-
+  cursor: pointer;
   transition-duration: 0.2s;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
 
