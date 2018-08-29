d8c22bab159deadfe151e869d867d865e3fe10c6
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-08-29 18:34:12 -0400

    Fix header on large displays

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
 import workout from '../assets/images/workout.jpg';
@@ -12,8 +12,8 @@ const StyledHeader = styled.div`
 const HeaderImage = styled.img`
   display: block;
   margin-top: -15%;
-  max-width: 100%;
-  
+  width: 100%;
+
   transform: scale(1.02);
   filter: blur(1px);
 `;
@@ -23,37 +23,41 @@ const Overlay = styled.div`
   width: 100%;
   height: 100%;
   top: 0;
-  
+
   background-color: rgba(0, 0, 0, 0.3);
-`
+`;
 
 const HeaderCallout = styled.h3`
   position: absolute;
   top: 50%;
   left: 50%;
   width: 100%;
-  
+
   display: block;
   margin: 0;
-  
+
   color: #fff;
   font-size: 7vw;
   font-weight: 500;
   text-align: center;
   text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.8);
   transform: translate(-50%, -50%);
-  
+
   @media screen and (min-width: 512px) {
     font-size: 2.5em;
   }
-`
+`;
 
-export default () => {
-  return (
-    <StyledHeader>
-      <HeaderImage src={workout} />
-      <Overlay />
-      <HeaderCallout>Enabling <b><i>YOU</i></b> to do more</HeaderCallout>
-    </StyledHeader>
-  )
-}
+export default () => (
+  <StyledHeader>
+    <HeaderImage src={workout} />
+    <Overlay />
+    <HeaderCallout>
+      Enabling{' '}
+      <b>
+        <i>YOU</i>
+      </b>{' '}
+      to do more
+    </HeaderCallout>
+  </StyledHeader>
+);
